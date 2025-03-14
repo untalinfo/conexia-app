a6dd0d4ff377f7181de40814b550c114570c063f
oscar Info <racso1607@gmail.com>
2025-03-14 09:21:22 -0500

    add post pages

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-import type { User, Post } from "@/types"
+import type { User, Post, Comment} from "@/types"
 
 const API_BASE_URL = "https://jsonplaceholder.typicode.com"
 
@@ -22,6 +22,30 @@ export async function getUserPosts(userId: string): Promise<Post[]> {
   const response = await fetch(`${API_BASE_URL}/users/${userId}/posts`)
   if (!response.ok) {
     throw new Error(`Failed to fetch posts for user with id ${userId}`)
+  }
+  return response.json()
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const response = await fetch(`${API_BASE_URL}/posts`)
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts")
+  }
+  return response.json()
+}
+
+export async function getPost(id: string): Promise<Post> {
+  const response = await fetch(`${API_BASE_URL}/posts/${id}`)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch post with id ${id}`)
+  }
+  return response.json()
+}
+
+export async function getPostComments(postId: string): Promise<Comment[]> {
+  const response = await fetch(`${API_BASE_URL}/posts/${postId}/comments`)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch comments for post with id ${postId}`)
   }
   return response.json()
 }
